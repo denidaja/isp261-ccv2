b421a3d02a2fa786afbfa9d08020042cecee920f
Daja <ddenidaja@gmail.com>
2022-07-20 02:13:50 +0200

    add branch

---
+++ core-customize/hybris/bin/custom/democmstypeaddon/acceleratoraddon/web/webroot/WEB-INF/views/responsive/cms/democmscomponent.jsp
@@ -0,0 +1,9 @@
+<script type="module">
+  import { LuigiContainer } from "./assets/luigi/bundle.js";
+
+  customElements.define("luigi-container", LuigiContainer);
+</script>
+
+<luigi-container viewUrl="{AppGyver_URL}"></luigi-container>
+<br />
+demoMessage = ${demoMessage}
---
+++ core-customize/hybris/bin/custom/democmstypeaddon/src/org/training/democmstypeaddon/constants/DemocmstypeaddonConstants.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2019 SAP SE or an SAP affiliate company. All rights reserved.
+ */
+package org.training.democmstypeaddon.constants;
+
+/**
+ * Global class for all Democmstypeaddon constants. You can add global constants for your extension into this class.
+ */
+
+public final class DemocmstypeaddonConstants extends GeneratedDemocmstypeaddonConstants
+{
+	public static final String EXTENSIONNAME = "democmstypeaddon";
+
+	private DemocmstypeaddonConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+}
